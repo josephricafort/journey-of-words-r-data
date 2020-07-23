20a2eaceb3ce9cfa15c89d5812bbd8b2a77a229b
josephricafort <josephricafortjr@gmail.com>
2020-07-23 19:51:15 +0800

    generate word count data

--- scripts/heirarchy.R
+++ scripts/heirarchy.R
@@ -60,8 +60,8 @@ language_heirarchy_scrape_data <- tibble(name_group, level_group, classification
 
 # Getting a better heirarchy in order
 
-branch_order <- c("Proto-Austronesian", "Formosan", "Philippine", "Malayo-Polynesian",
-                  "Western Malayo-Polynesian", "Central Malayo-Polynesian", "South Halmahera-West New Guinea",
+branch_order <- c("Proto-Austronesian", "Formosan", "Philippine", "Western Malayo-Polynesian", 
+                  "Central Malayo-Polynesian", "South Halmahera-West New Guinea",
                   "Papuan Tip", "North New Guinea", "Admiralty Islands", "Central-Eastern Oceanic", 
                   "Southeast Solomonic", "Meso Melanesian", "Temotu", "New Caledonian", 
                   "Micronesian", "North and Central Vanuatu","Northeast Vanuatu-Banks Islands",
@@ -99,7 +99,7 @@ gp_western_malayo_polynesian <- c("Celebic", "North Borneo", "Malayo-Chamic", "G
 # 14. Central Pacific (group 8)
 # 15. Polynesian (group 10) // under Central Pacific
 
-gp2 <- c("Eastern ", "Malayo-Polynesian")
+gp2 <- c("Eastern ",)
 gp3 <- c("Philippine")
 gp4 <- c("Central Malayo-Polynesian")
 gp5 <- c("South Halmahera-West New Guinea")
@@ -135,8 +135,19 @@ new_lang_heir_arr <- language_heirarchy_array %>%
   arrange(branch_id, id_lang) %>%
   left_join(language_info_api_clean %>% select(id_lang, latitude, longitude)) %>%
   # Remove Proto-Austronesian
-  filter(branch_id != 1)
-
+  filter(branch_id != 1) %>%
+  # move coordinates on the same side
+  mutate(longitude = ifelse(longitude < 0, longitude + 360, longitude))
+# Remove Malayo Polynesian umbrella and shift group2-14 after removing
+new_lang_heir_arr <- new_lang_heir_arr %>% filter(group2 == "Malayo-Polynesian") %>% select(-group2) %>%
+  rename_with(function(col){ 
+      num <- str_extract_all(col, "\\d+") %>% as.integer
+      return (paste0("group", num-1))
+    }, starts_with("group")) %>% rename(group1 = group0) %>%
+  bind_rows(
+    new_lang_heir_arr %>% filter(group2 != "Malayo-Polynesian")
+  ) %>% arrange(branch_id, language)
+  
 write_json(new_lang_heir_arr, "data/output/json/heirarchy/language_heirarchy_array.json", pretty=T)
 
 # Just a quick plot of how the data would look like
@@ -167,18 +178,37 @@ for (grp in 2:14){
   cntr_lang_groups <- bind_rows(cntr_lang_groups, result) %>% 
     arrange(name)
 }
-cntr_lang_groups <- cntr_lang_groups %>% bind_rows(
-  new_lang_heir_arr %>% group_by(branch) %>% 
-    summarize(lat = mean(latitude, na.rm=T),lon = mean(longitude, na.rm=T)) %>%
-    rename(name = branch)
-) %>% arrange(name)
+# cntr_lang_groups <- cntr_lang_groups %>% bind_rows(
+#   new_lang_heir_arr %>% group_by(branch) %>% 
+#     summarize(lat = mean(latitude, na.rm=T),lon = mean(longitude, na.rm=T)) %>%
+#     rename(name = branch)
+# ) %>% arrange(name)
+
+# Generate major lang groups
+cntr_major_groups <- new_lang_heir_arr %>% group_by(branch) %>% 
+  summarize(lat = mean(latitude, na.rm=T),lon = mean(longitude, na.rm=T))
+
+major_groups_flows <- tibble(
+  origin = character(),
+  dest = character()
+)
+for(row in 1:nrow(cntr_major_groups)){
+  origin <- cntr_major_groups$branch[row] %>% as.character
+  dest <- cntr_major_groups$branch[row + 1] %>% as.character
+  count <- 100
+  if(!is.na(dest)){
+    major_groups_flows <- bind_rows(major_groups_flows, tibble(origin, dest, count))
+  }
+}
 
 # Generate data for origin map
 heirarchy_location <- new_lang_heir_arr %>% select(id_lang, language, latitude, longitude) %>%
   rename(lat = latitude, lon = longitude, name = language) %>%
   bind_rows(cntr_lang_groups) %>%
   filter(!is.na(lat) | !is.na(lon)) %>%
-  mutate(node_id = row_number())
+  mutate(node_id = row_number()) %>%
+  # restructure according to flowmap requirement
+  select(node_id, name:lon) %>% rename(id = node_id)
 
 # Generate data for flow map
 heirarchy_flows <- tibble(
@@ -200,10 +230,20 @@ for(row in 1:nrow(new_lang_heir_arr)){
       # 2. group 2, 3, 4... n
       origin <- new_lang_heir_arr[row, paste0("group", col)] %>% as.character
       dest <- new_lang_heir_arr[row, paste0("group", col+1)] %>% as.character
+      if(is.na(origin) & is.na(dest)){
+        next
+      }
+      if(is.na(dest)){
+        dest <- language %>% as.character
+      }
     } else if (col == 14) { 
       # 3. group n -> language
       origin <- new_lang_heir_arr[row, paste0("group", col)] %>% as.character
-      dest <- language %>% as.character
+      if(!is.na(origin)){
+        dest <- language %>% as.character
+      } else {
+        dest <- NA
+      }
     }
     if(!is.na(dest)){
       heirarchy_flows <- bind_rows(heirarchy_flows, tibble(origin, dest))
@@ -212,18 +252,22 @@ for(row in 1:nrow(new_lang_heir_arr)){
   print(paste0("Row ", row, ": Done!"))
 }
 
-heir_orig_ref <- heirarchy_location %>% select(name, node_id)
+heir_orig_ref <- heirarchy_location %>% select(name, id)
 heirarchy_flows <- heirarchy_flows %>% group_by(origin, dest) %>% summarize(count = n()) %>% 
   arrange(desc(count)) %>% 
-  mutate(origin_id = heir_orig_ref$node_id[match(origin, heir_orig_ref$name)]) %>%
-  mutate(dest_id = heir_orig_ref$node_id[match(dest, heir_orig_ref$name)]) %>%
+  mutate(origin_id = heir_orig_ref$id[match(origin, heir_orig_ref$name)]) %>%
+  mutate(dest_id = heir_orig_ref$id[match(dest, heir_orig_ref$name)]) %>%
   select(origin, origin_id, dest, dest_id, count) %>%
   rename(origin_name = origin,
          origin = origin_id,
          dest_name = dest,
-         dest = dest_id)
-
+         dest = dest_id) %>%
+  filter(!is.na(origin)) %>% filter(!is.na(dest)) %>%
+  # restructure according to flowmap requirement
+  ungroup %>%
+  select(-origin_name, -dest_name)
+  
 write_json(heirarchy_location, "data/output/json/heirarchy/language_heirarchy_location.json", pretty=T)
 write_json(heirarchy_flows, "data/output/json/heirarchy/language_heirarchy_flows.json", pretty=T)
-write.csv(heirarchy_location, "data/output/csv/heirarchy/language_heirarchy_location.csv")
-write.csv(heirarchy_flows, "data/output/csv/heirarchy/language_heirarchy_flows.csv")
+write.csv(heirarchy_location, "data/output/csv/heirarchy/language_heirarchy_location.csv", row.names=F)
+write.csv(heirarchy_flows, "data/output/csv/heirarchy/language_heirarchy_flows.csv", row.names=F)
