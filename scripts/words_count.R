18ede5712111de75c3f8f51a4bb4903f000ec8aa
josephricafort <josephricafortjr@gmail.com>
2020-07-28 20:37:47 +0800

    add lat lang top20pc data, cleanup

--- scripts/words_count.R
+++ scripts/words_count.R
@@ -6,15 +6,26 @@ words_info_all
 language_words_count <- language_words_api_clean %>%
   group_by(item, word) %>% summarize(count = n(), cognacy1 = mean(cognacy1, na.rm=T)) %>% 
   mutate(cognate_group = 10*ceiling(cognacy1/10)) %>%
-  arrange(desc(count)) %>%
+  arrange(cognacy1, desc(count)) %>%
   left_join(words_info_all) %>% select(-entries) %>%
   mutate(word = str_replace_all(word, "\\,", ""),
          word = str_replace_all(word, " ", "_")) %>%
-  ungroup()
+  ungroup() %>%
+  filter(!is.na(cognacy1))
+
+# Top 20% count of every cognate group
+language_words_count_top20pc <- language_words_count %>% 
+  group_by(cognate_group) %>% top_frac(0.20, count)
 
 # Filter only top 20 per word
 language_words_count_top20 <- language_words_count %>% group_by(word) %>%
   slice_max(order_by = count, n = 20) %>%
+  filter(word != "")
+
+# Filter only top 200 per word
+language_words_count_top200 <- language_words_count %>% group_by(word) %>%
+  slice_max(order_by = count, n = 200) %>%
+  arrange(word, cognacy1, desc(count)) %>%
   filter(word != "")
 
 # Chop words data into multiple pieces for easy accessing for the API use
@@ -31,4 +42,14 @@ for(id in 1:length(word_list)){
   write_json(result, filedir)
 }
 
+write_json(language_words_count, "data/output/json/words_count.json")
 write_json(language_words_count_top20, "data/output/json/words_count_top20.json")
+write_json(language_words_count_top200, "data/output/json/words_count_top200.json")
+write_json(language_words_count_top20pc, "data/output/json/words_count_top20pc.json")
+
+write_json(language_words_count_top200 %>% filter(word == "eye"), "data/output/json/words_count_mata_sample.json")
+write_json(language_words_count_top200 %>% filter(word == "one"), "data/output/json/words_count_isa_sample.json")
+
+write_json(language_words_count %>% filter(word == "eye"), "data/output/json/words_count_mata_sample.json")
+write_json(language_words_count %>% filter(word == "one"), "data/output/json/words_count_isa_sample.json")
+write_json(language_words_count %>% filter(word == "five"), "data/output/json/words_count_lima_sample.json")
